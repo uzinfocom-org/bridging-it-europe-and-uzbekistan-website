741fb4569ea4f220772e8643277a607c0b93cdd5
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-10-01 17:34:05 +0500

    feat: new agenda

--- app/agenda-section.tsx
+++ app/agenda-section.tsx
@@ -1,46 +1,76 @@
 import React from "react";
-import Image from "next/image";
-import { Heading } from "./heading";
-import agendaTexts from "./agenda.json";
-import { AgendaTime as AgendaItem } from "./agenda-item";
+import { agenda, Speaker, Text } from "./agenda";
+
+const RenderText: React.FC<{ text: Text }> = ({ text }) => {
+  switch (text.type) {
+    case "text":
+      return <span className="text-xl font-bold text-[#D1CAFF]">{text.text}</span>
+    case "presentation":
+      return <><span className="text-xl text-[#D1CAFF]">{text.prefix}</span><span className="text-xl text-white font-bold">{text.name}</span></>
+  }
+}
+
+const RenderSpeaker: React.FC<{ speaker: Speaker }> = ({ speaker }) => {
+  switch (speaker.type) {
+    case "person":
+      return <div className="flex gap-2">
+        <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M23.6762 13.5002C23.6762 14.8299 23.4143 16.1465 22.9055 17.375C22.3967 18.6034 21.6508 19.7196 20.7106 20.6598C19.7704 21.6001 18.6542 22.3459 17.4258 22.8547C16.1973 23.3636 14.8807 23.6255 13.551 23.6255C12.2213 23.6255 10.9047 23.3636 9.67625 22.8547C8.4478 22.3459 7.33161 21.6001 6.39139 20.6598C5.45118 19.7196 4.70536 18.6034 4.19652 17.375C3.68768 16.1465 3.42578 14.8299 3.42578 13.5002C3.42578 10.8148 4.49254 8.23946 6.39139 6.34061C8.29024 4.44176 10.8656 3.375 13.551 3.375C16.2364 3.375 18.8118 4.44176 20.7106 6.34061C22.6095 8.23946 23.6762 10.8148 23.6762 13.5002Z" stroke="#FF57EE" stroke-width="1.68754" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M19.1762 21.9378C18.8195 14.9773 8.28254 14.9773 7.9259 21.9378M16.3636 10.4063C16.3636 11.1523 16.0673 11.8676 15.5398 12.3951C15.0124 12.9226 14.297 13.2189 13.551 13.2189C12.8051 13.2189 12.0897 12.9226 11.5622 12.3951C11.0348 11.8676 10.7385 11.1523 10.7385 10.4063C10.7385 9.66037 11.0348 8.94499 11.5622 8.41753C12.0897 7.89007 12.8051 7.59375 13.551 7.59375C14.297 7.59375 15.0124 7.89007 15.5398 8.41753C16.0673 8.94499 16.3636 9.66037 16.3636 10.4063Z" stroke="#FF57EE" stroke-width="1.68754" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+
+        <div className="flex flex-col">
+          <span className="text-[#F4C1F5] text-lg font-semibold">
+            {speaker.name}
+          </span>
+          <span className="text-[#F475F6]">
+            {speaker.title}
+          </span>
+        </div>
+      </div>
+    case "other":
+      return <div className="text-[#F4C1F5] text-lg font-semibold">{speaker.name}</div>
+  }
+}
 
 export const AgendaSection: React.FC = () => {
-  const breakIndex = agendaTexts.findIndex((text) => text.type == "break")
-  const texts1 = agendaTexts.slice(0, breakIndex)
-  const texts2 = agendaTexts.slice(breakIndex + 1)
-
   return (
     <section
       className="relative container mx-auto mt-36 mb-20 z-10 px-8 lg:px-0"
       id="agenda"
     >
-      <Heading lines text="Agenda" className="mb-16" />
+      <h1 className="text-8xl font-bold mb-16">Event Program</h1>
 
-      <div className="grid md:grid-cols-3 gap-6">
-        <div className="flex flex-col gap-8">
-          {texts1.map((details, index) => (
-            <AgendaItem key={index} {...details} />
-          ))}
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+        <div className="md:col-span-2 text-[#FEBCFF]/50 bg-[#FEBCFF]/5 px-2.5 py-3.5">
+          Time
         </div>
-        <Image
-          className="pointer-events-none"
-          src="/agenda-shape.png"
-          alt="Glass shape"
-          width={485}
-          height={479}
-        />
-        <div className="flex flex-col gap-8">
-          {texts2.map((details, index) => (
-            <AgendaItem key={index} {...details} />
-          ))}
+        <div className="md:col-span-5 text-[#FEBCFF]/50 bg-[#FEBCFF]/5 px-2.5 py-3.5">
+          Event
         </div>
-      </div>
-
-      <div className="relative">
-        <div className="mt-4 xs:mt-0 xs:absolute left-0 bottom-0 inline-flex border border-[#371E51] text-[#AEA2FF] p-6 rounded-4xl">
-          (Agenda times may be adjusted; final program will be shared with
-          confirmed attendees.)
+        <div className="md:col-span-5 text-[#FEBCFF]/50 bg-[#FEBCFF]/5 px-2.5 py-3.5">
+          Speaker / Details
         </div>
+        <div className="md:col-span-12 h-5" />
+        {agenda.map((agenda, index) => <React.Fragment key={index}>
+          {index != 0 && <div className="bg-[image:var(--line-gradient)] md:col-span-12 h-px my-6 md:my-2" />}
+          <div className="md:col-span-2">
+            <div className="text-3xl font-bold text-[#F6EEFF]">
+              {agenda.start}
+            </div>
+            <div className="text-2xl font-medium text-[#79739D]">
+              {agenda.end}
+            </div>
+          </div>
+          <div className="md:col-span-5">
+            <RenderText text={agenda.text} />
+          </div>
+          <div className="md:col-span-5 flex gap-6 flex-wrap">
+            {agenda.speakers.map((speaker, index) => (
+              <RenderSpeaker key={index} speaker={speaker} />
+            ))}
+          </div>
+        </React.Fragment>)}
       </div>
     </section>
   );
---
+++ app/agenda.ts
@@ -0,0 +1,249 @@
+export type Speaker = {
+  type: "person",
+  name: string,
+  title?: string
+} | {
+  type: "other",
+  name: string
+}
+
+export type Text = {
+  type: "text",
+  text: string
+} | {
+  type: "presentation",
+  prefix: string,
+  name: string
+}
+
+export type AgendaLine = {
+  start: string,
+  end: string,
+  text: Text,
+  speakers: Speaker[]
+}
+
+const timurRakhmanov: Speaker = {
+  type: "person",
+  name: "Timur Rakhmanov",
+  title: "Ambassador of Uzbekistan to Latvia"
+}
+
+const aliaksandrPiatrou: Speaker = {
+  type: "person",
+  name: "Aliaksandr Piatrou",
+  title: "Head of the Europa, UZINFOCOM EUROPE"
+}
+
+const viktoriayaDubova: Speaker = {
+  type: "person",
+  name: "Viktoriya Dubova",
+  title: "Head of Strategic Partnerships, UZINFOCOM EUROPE"
+}
+
+const umidSunnatjonov: Speaker = {
+  type: "person",
+  name: "Umid Sunnatjonov",
+  title: "Head of Business Development in Europe, UZINFOCOM"
+}
+
+const timurMiyassarov: Speaker = {
+  type: "person",
+  name: "Timur Miyassarov",
+  title: "Member of the Board, UZINFOCOM"
+}
+
+const temurBatyrov: Speaker = {
+  type: "person",
+  name: "Temur Batyrov",
+  title: "Director, Global Solutions Office, UZINFOCOM"
+}
+
+const ruslansArzaniks: Speaker = {
+  type: "person",
+  name: "Ruslans Arzaniks",
+  title: "CTO, XInfotech"
+}
+
+export const agenda: AgendaLine[] = [
+  {
+    start: "11:30",
+    end: "12:00",
+    text: {
+      type: "text",
+      text: "Guest Arrival"
+    },
+    speakers: [],
+  },
+  {
+    start: "12:00",
+    end: "12:30",
+    text: {
+      type: "text",
+      text: "Welcome Coffee"
+    },
+    speakers: [],
+  },
+  {
+    start: "12:30",
+    end: "12:40",
+    text: {
+      type: "text",
+      text: "Opening Remarks"
+    },
+    speakers: [
+      timurRakhmanov,
+      aliaksandrPiatrou
+    ],
+  },
+  {
+    start: "12:40",
+    end: "12:50",
+    text: {
+      type: "text",
+      text: "UZINFOCOM Company Presentation (Video)"
+    },
+    speakers: [],
+  },
+  {
+    start: "12:50",
+    end: "13:10",
+    text: {
+      type: "presentation",
+      prefix: "Presentation: ",
+      name: "Your Gateway to Digital Excellence"
+    },
+    speakers: [
+      viktoriayaDubova
+    ],
+  },
+  {
+    start: "13:10",
+    end: "13:20",
+    text: {
+      type: "text",
+      text: "Q&A and Networking"
+    },
+    speakers: [],
+  },
+  {
+    start: "13:20",
+    end: "13:40",
+    text: {
+      type: "presentation",
+      prefix: "Presentation: ",
+      name: "Redefining Outsourcing"
+    },
+    speakers: [
+      umidSunnatjonov
+    ],
+  },
+  {
+    start: "13:40",
+    end: "13:50",
+    text: {
+      type: "text",
+      text: "Q&A and Networking"
+    },
+    speakers: [],
+  },
+  {
+    start: "13:50",
+    end: "14:10",
+    text: {
+      type: "presentation",
+      prefix: "Presentation: ",
+      name: "Remote Biometric Identification and Biometric Payments"
+    },
+    speakers: [
+      timurMiyassarov
+    ],
+  },
+  {
+    start: "14:10",
+    end: "14:20",
+    text: {
+      type: "text",
+      text: "Q&A and Networking"
+    },
+    speakers: [],
+  },
+  {
+    start: "14:20",
+    end: "15:00",
+    text: {
+      type: "text",
+      text: "Coffee Break & Light Refreshments"
+    },
+    speakers: [],
+  },
+  {
+    start: "15:00",
+    end: "15:20",
+    text: {
+      type: "presentation",
+      prefix: "Presentation: ",
+      name: "Technical Overview and Advantages of UZINFOCOM"
+    },
+    speakers: [
+      temurBatyrov
+    ],
+  },
+  {
+    start: "15:20",
+    end: "15:30",
+    text: {
+      type: "text",
+      text: "Q&A and Networking"
+    },
+    speakers: [],
+  },
+  {
+    start: "15:30",
+    end: "16:00",
+    text: {
+      type: "presentation",
+      prefix: "Presentation: ",
+      name: "Remote registration of non-EU residents, as a first step for opening of businesses in EU countries"
+    },
+    speakers: [
+      ruslansArzaniks
+    ],
+  },
+  {
+    start: "16:00",
+    end: "16:30",
+    text: {
+      type: "presentation",
+      prefix: "Panel discussion: ",
+      name: "IT Outsourcing: Challenges and Opportunities"
+    },
+    speakers: [
+      { ...timurMiyassarov, title: undefined },
+      { ...temurBatyrov, title: undefined },
+      { ...ruslansArzaniks, title: undefined },
+    ],
+  },
+  {
+    start: "16:30",
+    end: "17:00",
+    text: {
+      type: "text",
+      text: "Memorandum of Understanding (MoU) Signing Ceremony"
+    },
+    speakers: [
+      { type: "other", name: "UZINFOCOM EUROPE & H3C" }
+    ],
+  },
+  {
+    start: "17:00",
+    end: "20:00",
+    text: {
+      type: "text",
+      text: "Closing Dinner with Traditional Uzbek Cuisine"
+    },
+    speakers: [
+      { type: "other", name: "Informal Discussion & Networking" }
+    ],
+  },
+]
